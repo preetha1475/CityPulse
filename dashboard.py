4bc85cedbbd1ed6e890f125a10ae9785604a0d58
preetha1475 <pretha1475@gmail.com>
2026-03-31 17:43:04 +0530

    Update fmt.Println message from 'Hello' to 'Goodbye'

--- dashboard.py
+++ dashboard.py
@@ -1275,7 +1275,7 @@ elif page == "Public Sentiment Analysis":
     # ---------------------------------------------
     # Load Survey Dataset
     # ---------------------------------------------
-    SURVEY_PATH = "data/file/Metro impact responses.xlsx"
+    SURVEY_PATH = pd.read_csv("data/Metro impact responses.xlsx")
     @st.cache_data
     def load_survey():
         df = pd.read_excel(SURVEY_PATH)
