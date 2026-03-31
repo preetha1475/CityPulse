bee691060f0364b2840e930457b77f69294cd7ae
preetha1475 <pretha1475@gmail.com>
2026-03-31 17:52:29 +0530

    Update print statement from 'Hello' to 'Goodbye'

--- dashboard.py
+++ dashboard.py
@@ -1561,7 +1561,7 @@ elif page == "Metro Safety Intelligence":
     # LOAD ACCIDENT DATASET
     # =========================================
 
-    ACCIDENT_PATH = r"\metro_traffic_project\data\RTA Dataset.csv"
+    ACCIDENT_PATH = r"data\RTA Dataset.csv"
 
     @st.cache_data
     def load_accident_data():
